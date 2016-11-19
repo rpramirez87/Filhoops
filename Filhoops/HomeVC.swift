9109c97ba71255ea1418acbc09534be6cfbe83f7
ramirezr69 <ronramirez@patricks-imac.attlocal.net>
2016-11-19 17:15:30 -0600

    Set up tabs in UITabBarController, Initialize first button, Home Button as the Filhoops Homepage.

---
+++ Filhoops/HomeVC.swift
@@ -0,0 +1,49 @@
+//
+//  HomeVC.swift
+//  Filhoops
+//
+//  Created by Ron Ramirez on 11/19/16.
+//  Copyright © 2016 Mochi Apps. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class HomeVC: UIViewController, WKNavigationDelegate {
+    
+    @IBOutlet weak var container : UIView!
+    var webView : WKWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // loading URL :
+        let filhoopsURL = "http://filhoopshouston.com/"
+        let url = NSURL(string: filhoopsURL)
+        let request = NSURLRequest(url: url! as URL)
+        
+        // init and load request in webview.
+        webView = WKWebView(frame: self.view.frame)
+        webView.navigationDelegate = self
+        webView.load(request as URLRequest)
+        self.view.addSubview(webView)
+        self.view.sendSubview(toBack: webView)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    //MARK:- WKNavigationDelegate
+    
+    func webView(_ webView: WKWebView, didFailProvisionalNavigation navigation: WKNavigation!, withError error: Error) {
+        print(error.localizedDescription)
+    }
+    func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+        print("Strat to load")
+    }
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        print("finish to load")
+    }
+}
